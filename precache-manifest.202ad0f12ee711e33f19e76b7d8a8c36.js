0dd53284b7e98c4ac79aad7e3d8cdd21cbf1df74
meo098 <meo098@o2.pl>
2020-10-16 19:32:19 +0200

    Updates

---
+++ precache-manifest.202ad0f12ee711e33f19e76b7d8a8c36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bb7490259ec00d453b0c4b61882121b",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "f1b1401fc875759814a3",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5a46617966b536919ab2",
+    "url": "/my-app/static/js/2.68f56c50.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/my-app/static/js/2.68f56c50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1b1401fc875759814a3",
+    "url": "/my-app/static/js/main.e320ead7.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
